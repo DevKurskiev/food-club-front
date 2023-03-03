407dde632db282a080108e7fcff5c10b0c34cbcb
Adam Kurskiev <devkurskiev@gmail.com>
2023-03-03 22:32:23 +0300

    change proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/",
+    "/api",
     createProxyMiddleware({
       target: "http://89.108.81.191:8081",
       changeOrigin: true,
